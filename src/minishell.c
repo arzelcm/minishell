c76653cd1012af46fecea3443b1f4ffaf44976f6
arzelcm <arzelcanavate@gmail.com>
2024-05-07 14:30:18 +0200

    Fix

--- src/minishell.c
+++ src/minishell.c
@@ -6,24 +6,26 @@
 /*   By: arcanava <arcanava@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 20:08:54 by arcanava          #+#    #+#             */
-/*   Updated: 2024/05/07 14:21:58 by arcanava         ###   ########.fr       */
+/*   Updated: 2024/05/07 14:29:43 by arcanava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <readline/readline.h>
+ #include <readline/history.h>
 
 int	main(int argc, char **argv, char **envp)
 {
+	char	*curr_line;
+
 	(void) argv;
 	(void) envp;
 	if (argc > 1)
 		return (EXIT_FAILURE);
-	ft_printf("Hello amatist!\n");
 	while (42)
 	{
-		char *newline = readline("Amatist> ");
-		ft_printf("Read: %s\n", newline);
+		curr_line = readline("Amatist> ");
+		free(curr_line);
 	}
 	return (EXIT_SUCCESS);
 }
