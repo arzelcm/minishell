746ac846ef14a2d5b64cd6be321762fd69c54f4b
arzelcm <arzelcanavate@gmail.com>
2024-05-15 20:26:46 +0200

    Delete typedef alone definitions

--- inc/token.h
+++ inc/token.h
@@ -1,51 +1,47 @@
 #ifndef TOKEN_H
 # define TOKEN_H
 
-typedef enum e_token_types		t_token_types;
-typedef enum e_redirection_mode	t_redirection_mode;
-typedef struct s_tokens			t_tokens;
-typedef struct s_token			t_token;
-typedef struct s_redirection	t_redirection;
-
-enum e_token_types
+typedef enum e_token_type
 {
-	CMD
-};
+	CMD,
+	PIPE
+}	t_token_type;
 
-enum e_redirection_mode
+typedef enum e_redirection_mode
 {
 	INPUT,
 	OUTPUT,
 	HERE_DOC,
 	APPEND
-};
+}	t_redirection_mode;
 
-struct s_redirection
+typedef struct s_redirection
 {
 	union
 	{
-		char			*path;
-		char			*delimiter;
+		char	*path;
+		char	*delimiter;
 	};
-	t_redirection_mode	mode;
-	t_redirection		*next;
-};
+	t_redirection_mode		mode;
+	struct s_redirection	*next;
+}	t_redirection;
 
-struct s_tokens
+typedef struct s_token	t_token;
+typedef struct s_tokens
 {
 	t_token	*token;
 	int		amount;
-};
+}	t_tokens;
 
-struct s_token
+typedef struct s_token
 {
-	t_token_types	type;
+	t_token_type	type;
 	char			*raw_token;
 	char			**args;
 	t_redirection	*infiles;
 	t_redirection	*outfiles;
 	t_redirection	*here_docs;
 	t_tokens		*tokens;
-};
+}	t_token;
 
 #endif
