dd483714f340cd358825d616634ba9cc3362f269
arzelcm <arzelcanavate@gmail.com>
2024-05-30 14:51:50 +0200

    Add some refactor

--- src/executor/builtins/export.c
+++ src/executor/builtins/export.c
@@ -10,7 +10,7 @@
 // TODO: Leaks
 // TODO: Search duplicates on local or global and replace when needed, NONE CAN BE DUPLICATED
 
-static void	fill_definitions(char **definitions, int *i, char **envp, int size)
+static void	fill_definitions(char **definitions, int *i, char **envp)
 {
 	char	*key;
 	char	*value;
@@ -19,16 +19,15 @@ static void	fill_definitions(char **definitions, int *i, char **envp, int size)
 	char	*key_value;
 	int		j;
 
-	(void) size;
 	j = 0;
 	while (envp[j])
 	{
-		// ft_printf("%.2i: %s\n", *i, envp[j]);
 		if (ft_stroccurrences(envp[j], '='))
 		{
 			complete = ft_split(envp[j], '=');
 			key = complete[0];
 			value = complete[1];
+			free(complete);
 			if (value)
 			{
 				complete_val = ft_strjoin("=\"", value);
@@ -48,21 +47,18 @@ static void	fill_definitions(char **definitions, int *i, char **envp, int size)
 		else
 			key_value = ft_strdup(envp[j]);
 		j++;
-		definitions[*i] = ft_strjoin("declare -x ", key_value);
-		// ft_printf("%.2i: %s\n", *i, definitions[*i]);
-		(*i)++;
+		definitions[(*i)++] = ft_strjoin("declare -x ", key_value);
 		free(key_value);
 	}
 	definitions[*i] = NULL;
 }
 
-static void	sort_definitions(char **definitions, int size)
+static void	sort_definitions(char **definitions)
 {
 	int		i;
 	int		j;
 	char	*aux;
 
-	(void) size;
 	i = 0;
 	while (definitions[i])
 	{
@@ -88,9 +84,9 @@ static void	print_definitions(t_context *context)
 
 	definitions = safe_calloc(sizeof(char *) * (context->global_env.size + context->local_env.size + 1));
 	i = 0;
-	fill_definitions(definitions, &i, context->global_env.envp, context->global_env.size);
-	fill_definitions(definitions, &i, context->local_env.envp, context->local_env.size);
-	sort_definitions(definitions, context->global_env.size + context->local_env.size);
+	fill_definitions(definitions, &i, context->global_env.envp);
+	fill_definitions(definitions, &i, context->local_env.envp);
+	sort_definitions(definitions);
 	print_env(definitions);
 	free_matrix(definitions);
 }
