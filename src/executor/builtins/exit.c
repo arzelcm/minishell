4db217ab290d06065e3d3f5e6170855a1b76fc22
arzelcm <arzelcanavate@gmail.com>
2024-06-01 00:34:14 +0200

    Fix exit

--- src/executor/builtins/exit.c
+++ src/executor/builtins/exit.c
@@ -1,12 +1,20 @@
+#include "minishell.h"
 #include "libft.h"
 #include "context.h"
 #include "environment_helper.h"
 
 int	ft_exit(int argc, char **argv, t_context *context)
 {
-	(void) argc;
-	(void) argv;
+	if (argc > 2)
+	{
+		ft_printff(STDERR_FILENO, "%s: exit: too many arguments\n", PROGRAM_NAME);
+		context->err_code = EXIT_FAILURE;
+		return (1);
+ 	}
+	if (argc == 2)
+		context->err_code = ft_atoi(argv[1]);
 	free_enviroment(&context->global_env);
 	free_enviroment(&context->local_env);
-	exit(EXIT_SUCCESS);
+	ft_printff(STDERR_FILENO, "exit\n");
+	exit(context->err_code);
 }
