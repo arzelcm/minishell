a2ad15026ebfa7871d06333cd20f84d067776a5b
arzelcm <arzelcanavate@gmail.com>
2024-06-19 03:15:43 +0200

    Add redirection expanse

--- inc/tokenizer_redirections.h
+++ inc/tokenizer_redirections.h
@@ -8,4 +8,6 @@ void	free_redirections(t_redirection *list);
 
 int		set_redirection(char *line, int *i, t_token *token);
 
+char	*expand_redirect(t_redirection *redirection, t_context *context);
+
 #endif
