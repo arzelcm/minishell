80ada368c53016124846a5c3223f5abd48286b1c
cfareste <chrisfidare@yahoo.com>
2024-07-03 12:37:35 +0200

    Temporary fix

--- src/executor/open_utils.c
+++ src/executor/open_utils.c
@@ -71,32 +71,32 @@ int	open_infile(char *path)
 	return (fd);
 }
 
-static int	check_outfile(char *path)
-{
-	char	*prefix;
-	int		prefix_len;
+// static int	check_outfile(char *path)
+// {
+// 	char	*prefix;
+// 	int		prefix_len;
 
-	if (!path[0])
-		return (handle_error(path, NOFILEDIR), 0);
-	if (!ft_strchr(path, '/'))
-		return (1);
-	prefix_len = ft_strlen(path) - (ft_strlen(ft_strrchr(path, '/') + 1));
-	prefix = safe_calloc(prefix_len + 1);
-	ft_strlcpy(prefix, path, prefix_len + 1);
-	ft_printf("Prefix: %s\n", prefix);
-	if (access(prefix, F_OK) == -1 && !is_directory(prefix))
-	{
-		free(prefix);
-		return (handle_error(path, NOFILEDIR), 0);
-	}
-	if (!is_directory(path) || 
-		(!is_directory(path) && ft_strcmp(prefix, "./") != EQUAL_STRINGS))
-	{
-		free(prefix);
-		return (handle_error(path, NOTDIRECTORY), 0);
-	}
-	return (free(prefix), 1);
-}
+// 	if (!path[0])
+// 		return (handle_error(path, NOFILEDIR), 0);
+// 	if (!ft_strchr(path, '/'))
+// 		return (1);
+// 	prefix_len = ft_strlen(path) - (ft_strlen(ft_strrchr(path, '/') + 1));
+// 	prefix = safe_calloc(prefix_len + 1);
+// 	ft_strlcpy(prefix, path, prefix_len + 1);
+// 	ft_printf("Prefix: %s\n", prefix);
+// 	if (access(prefix, F_OK) == -1 && !is_directory(prefix))
+// 	{
+// 		free(prefix);
+// 		return (handle_error(path, NOFILEDIR), 0);
+// 	}
+// 	if (!is_directory(path) || 
+// 		(!is_directory(path) && ft_strcmp(prefix, "./") != EQUAL_STRINGS))
+// 	{
+// 		free(prefix);
+// 		return (handle_error(path, NOTDIRECTORY), 0);
+// 	}
+// 	return (free(prefix), 1);
+// }
 
 int	open_outfile(char *path, int mode)
 {
@@ -104,8 +104,8 @@ int	open_outfile(char *path, int mode)
 	int		flags;
 	char	*quoted_file;
 
-	if (!check_outfile(path))
-		return (-1);
+	// if (!check_outfile(path))
+	// 	return (-1);
 	if (is_directory(path))
 		return (handle_error(path, ISDIRECTORY_U), -1);
 	if (access(path, F_OK) == 0 && access(path, W_OK) == -1)
