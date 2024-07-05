279ea67e67736df11b581bcb4dc654bbfc8e7a07
arzelcm <arzelcanavate@gmail.com>
2024-07-05 16:31:48 +0200

    Add non-functional test

--- tests/shlvl.sh
+++ tests/shlvl.sh
@@ -1,4 +1,13 @@
-echo "export SHLVL=-1;
-echo $SHLVL" > shlvltest
-./minishell < shlvltest > shlvlres;
-cat shlvlres;
+# echo "export SHLVL=-1;
+# echo $SHLVL" > shlvltest
+# ./minishell < shlvltest > shlvlres;
+# cat shlvlres;
+
+test "export SHLVL=-1\necho"hola""
+
+test() {
+	echo "$1" >.own_test
+	./minishell < .own_test > .own_test_res;
+	cat .own_test_res;
+	# rm -f .own_test .own_test_res;
+}
