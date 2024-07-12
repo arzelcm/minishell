5a52973b311f6125eaaaaad91eaa50c0057dd324
arzelcm <arzelcanavate@gmail.com>
2024-07-12 19:39:04 +0200

    Add tokenizer for all bonuses

--- src/tokenizer/family_helper.c
+++ src/tokenizer/family_helper.c
@@ -6,7 +6,7 @@
 /*   By: arcanava <arcanava@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 20:10:20 by arcanava          #+#    #+#             */
-/*   Updated: 2024/07/12 11:38:52 by arcanava         ###   ########.fr       */
+/*   Updated: 2024/07/12 19:38:02 by arcanava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,60 +29,46 @@ void	replace_last_token(t_token *parent, t_token *token)
 	token->parent = parent;
 }
 
-void	create_parent(t_token **current, t_token *new_parent,
-	t_token **parent, int new_cmd)
+void
+	push_first_child(t_token **current, t_token **parent, t_token *new_parent)
 {
-	t_token	*old_parent;
+	t_token	*eval_token;
+	t_token	*new_eval;
 
-	old_parent = (*current)->parent;
-	if (old_parent)
-	{
-		if (old_parent->type >= new_parent->type)
-			replace_last_token(old_parent, new_parent);
-		else if (old_parent->type < new_parent->type)
-		{
-			if (old_parent->parent)
-				replace_last_token(old_parent->parent, new_parent);
-			push_token(new_parent, old_parent);
-		}
-	}
-	else if (*parent == NULL || (*parent == old_parent && old_parent->type < new_parent->type))
-	{
+	eval_token = *current;
+	new_eval = eval_token;
+	while (new_eval->type != LIST
+		&& new_eval->parent && new_eval->parent->type != SUBSHELL)
+		new_eval = new_eval->parent;
+	eval_token = new_eval;
+	if (eval_token->parent && eval_token->parent->type < new_parent->type)
+		replace_last_token(eval_token->parent, new_parent);
+	push_token(new_parent, eval_token);
+	if (*parent == NULL || (*parent == eval_token))
 		*parent = new_parent;
-	}
-	if (!old_parent || old_parent->type >= new_parent->type)
-		push_token(new_parent, *current);
-	if (new_cmd)
-	{
-		*current = new_token(CMD);
-		push_token(new_parent, *current);
-	}
 }
 
 int	set_list(char *line, int *i, t_token **parent, t_token **current)
 {
+	t_token	*new_parent;
+	int		line_not_finish;
 	int		is_and;
 	int		is_or;
-	int		line_len;
-	t_token	*list;
 
-	(void) current;
-	line_len = ft_strlen(line);
-	is_and = *i + 1 < line_len && line[*i] == '&' && line[*i + 1] == '&';
-	is_or = *i + 1 < line_len && !is_and
-		&& line[*i] == '|' && line[*i + 1] == '|';
-	if (is_and || is_or)
-	{
-		list = new_token(LIST);
-		list->dependency = AND;
-		if (is_or)
-			list->dependency = OR;
-		create_parent(current, list, parent, 1);
-		(*i) += 2;
-		return (1);
-	}
-	else
+	line_not_finish = *i + 1 < (int) ft_strlen(line);
+	is_and = line_not_finish && line[*i] == '&' && line[*i + 1] == '&';
+	is_or = line_not_finish && line[*i] == '|' && line[*i + 1] == '|';
+	if (!is_and && !is_or)
 		return (0);
+	new_parent = new_token(LIST);
+	new_parent->dependency = AND;
+	if (is_or)
+		new_parent->dependency = OR;
+	push_first_child(current, parent, new_parent);
+	*current = new_token(CMD);
+	push_token(new_parent, *current);
+	(*i) += 2;
+	return (1);
 }
 
 int	set_pipe(char *line, int *i, t_token **parent, t_token **current)
@@ -93,14 +79,14 @@ int	set_pipe(char *line, int *i, t_token **parent, t_token **current)
 	if (line[*i] != '|')
 		return (0);
 	old_parent = (*current)->parent;
-	if (!old_parent || old_parent->type < PIPE)
+	if (!old_parent || old_parent->type != PIPE)
 	{
 		new_parent = new_token(PIPE);
 		replace_last_token((*current)->parent, new_parent);
 		(*current)->parent = new_parent;
 		if (*parent == *current)
 			*parent = (*current)->parent;
-		push_token((*current)->parent, *current);
+		push_token(new_parent, *current);
 	}
 	push_token((*current)->parent, new_token(CMD));
 	*current = (*current)->next;
@@ -127,6 +113,8 @@ int	set_subshell(char *line, int *i, t_token **parent, t_token **current)
 		*current = (*current)->parent;
 		while ((*current)->type != SUBSHELL && (*current)->parent)
 			*current = (*current)->parent;
+		if (*parent == (*current)->tokens.token)
+			*parent = *current;
 	}
 	else
 		return (0);
@@ -136,12 +124,12 @@ int	set_subshell(char *line, int *i, t_token **parent, t_token **current)
 
 int	set_parent(char *line, int *i, t_token **parent, t_token **current)
 {
-	if (set_pipe(line, i, parent, current))
+	if (set_subshell(line, i, parent, current))
 		return (1);
-	else if (set_subshell(line, i, parent, current))
+	else if (set_list(line, i, parent, current))
 		return (1);
-	// else if (set_list(line, i, parent, current))
-	// 	return (1);
+	else if (set_pipe(line, i, parent, current))
+		return (1);
 	else
 		return (0);
 }
